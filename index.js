e5ca8a6b682def18bee3c4157ac0e30e02170aa3
ETEMESI <samweletemesi2@Gmail.com>
2024-01-05 22:55:10 +0300

    I did some practise on this lab to get a clear understanding of strings.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Write your code in this file!
-const currentUser = 'Grace Hopper';
+const currentUser = 'Samwel Etemesi';
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+// const welcomeMessage = 'Welcome to Flatbook,' + currentUser + '!';
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
